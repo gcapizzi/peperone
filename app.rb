7437777691803aa54e3970adbaf8ede2ebe5ac53
Giuseppe Capizzi <g.capizzi@gmail.com>
2014-07-28 14:23:45 +0200

    Formatting

--- app.rb
+++ app.rb
@@ -1,21 +1,21 @@
 require 'sinatra'
 
 get '/' do
-   erb :"peperones/view"
+  erb :"peperones/view"
 end
 
 get '/peperones' do
-   erb :"peperones/view"
+  erb :"peperones/view"
 end
 
 get '/short-breaks' do
-   erb :"short-breaks/view"
+  erb :"short-breaks/view"
 end
 
 get '/long-breaks' do
-   erb :"long-breaks/view"
+  erb :"long-breaks/view"
 end
 
 post '/peperones' do
-   erb :"peperones/view"
+  erb :"peperones/view"
 end
